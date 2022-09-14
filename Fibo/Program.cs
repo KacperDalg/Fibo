826762dbbf1a71ad83e0222d350c2b111e58e9c3
Kacper <kacperdalgiewicz@gmail.com>
2022-09-14 20:04:53 +0200

    update

--- Fibo/Program.cs
+++ Fibo/Program.cs
@@ -1,18 +1,19 @@
-﻿class Program
+﻿namespace Fibo;
+class Program
 {
-    static int fibo(int a)
+    static int CalculateFibonacciNumber(int input)
     {
-        int[] f = new int[a + 2];
+        int[] f = new int[input + 2];
 
         f[0] = 0;
         f[1] = 1;
 
-        for (int i = 2; i <= a; i++)
+        for (int i = 2; i <= input; i++)
         {
             f[i] = f[i - 1] + f[i - 2];
         }
 
-        return f[a];
+        return f[input];
     }
 
     static void Main()
@@ -28,19 +29,18 @@
         {
             Console.Clear();
             Console.WriteLine("Który wyraz ciągu Fibonacciego chcesz sprawdzić: ");
-            int x = int.Parse(Console.ReadLine());
-            Console.WriteLine($"{x}. wyrazem ciągu Fibonacciego jest liczba: {fibo(x)}");
+            int userInput = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{userInput}. wyrazem ciągu Fibonacciego jest liczba: {CalculateFibonacciNumber(userInput)}");
         }
         else
         {
             Console.Clear();
             string path = "file.txt";
-            StreamReader sr = new StreamReader(path);
-            int y = int.Parse(sr.ReadLine());
-            Console.WriteLine($"Wartość wczytana z pliku: {y}");
-            Console.WriteLine($"{y}. wyrazem ciągu Fibonacciego jest liczba: {fibo(y)}");
-            sr.Close();
+            StreamReader streamReader = new StreamReader(path);
+            int fileContent = int.Parse(streamReader.ReadLine());
+            Console.WriteLine($"Wartość wczytana z pliku: {fileContent}");
+            Console.WriteLine($"{fileContent}. wyrazem ciągu Fibonacciego jest liczba: {CalculateFibonacciNumber(fileContent)}");
+            streamReader.Close();
         }
-        
     }
 }
